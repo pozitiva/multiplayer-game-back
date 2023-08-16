76505b3084c4a4a3432493f836b8a01fcda5d088
pozitiva <ivadjokovic2@gmail.com>
2023-08-16 20:41:28 +0200

    login i registracija

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET =
+  "cnsdbcfdsbfhs[]werp4358394ds[fsdjbvvsfdkjvdsbgkw=w]3pr2353879764253+_P>POopkdfe4";
+
+mongoose.connect(
+  "mongodb+srv://iva:iva@cluster0.muybin8.mongodb.net/?retryWrites=true&w=majority"
+);
+mongoose.connection.on("connected", () => console.log("Connected to db"));
+
+app.get("/", (req, res) => {
+  res.send("evo malo blejica na homepage");
+});
+
+require("./userDetails");
+const User = mongoose.model("UserInfo");
+app.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.send({ error: "User already exists" });
+    }
+    await User.create({
+      fname,
+      lname,
+      email,
+      password: encryptedPassword,
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
+  }
+});
+
+app.post("/login-user", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.send({ error: "User not found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ email: user.email }, JWT_SECRET);
+
+    if (res.status(201)) {
+      return res.json({ status: "ok", data: token });
+    } else {
+      return res.json({ erro: "error" });
+    }
+  }
+  res.json({ status: "error", error: "Invalid Password" });
+});
+
+app.post("/userData", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    console.log(user);
+
+    const useremail = user.email;
+    User.findOne({ email: useremail })
+      .then((data) => {
+        res.send({ status: "ok", data: data });
+      })
+      .catch((error) => {
+        res.send({ status: "error", data: error });
+      });
+  } catch (error) {}
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
